274a1968d993da5c4002d9dd644415f471305144
willem <himself@willemthiart.com>
2014-02-09 03:10:38 +1300

    Separated struct definitions into header file

--- pwp_connection.c
+++ pwp_connection.c
@@ -29,6 +29,7 @@
 #include "sparse_counter.h"
 #include "bitstream.h"
 #include "meanqueue.h"
+#include "pwp_connection_private.h"
 
 #define TRUE 1
 #define FALSE 0
@@ -43,72 +44,6 @@
     PWP_MSGTYPE_REQUEST == (m) ? "REQUEST" :\
     PWP_MSGTYPE_PIECE == (m) ? "PIECE" :\
     PWP_MSGTYPE_CANCEL == (m) ? "CANCEL" : "none"\
-
-/*  state */
-typedef struct
-{
-    /* TODO: this should be removed */
-    /* this bitfield indicates which pieces the peer has */
-    bitfield_t have_bitfield;
-
-    /* for recording state machine's state */
-    unsigned int flags;
-
-    /* count number of failed connections */
-    int failed_connections;
-
-    /* current tick */
-    int tick;
-
-} peer_connection_state_t;
-
-typedef struct
-{
-    /* the tick which this request was made */
-    int tick;
-    bt_block_t blk;
-} request_t;
-
-/*  peer connection */
-typedef struct
-{
-    peer_connection_state_t state;
-
-    unsigned int bytes_downloaded_this_period,
-                 bytes_uploaded_this_period;
-
-    /* Download/upload rate measurement */
-    void *bytes_drate,
-        *bytes_urate;
-
-    /* Pending requests that we are waiting to get
-     * We could receive pieces that are a subset of the original request */
-    hashmap_t *recv_reqs;
-
-    /* Pending requests we are fufilling for the peer */
-    linked_list_queue_t *peer_reqs;
-    
-    /* list of requests to make */
-    linked_list_queue_t *reqs;
-    void *req_lock;
-
-    // TODO: need to remove this
-    /* need the piece_length to check pieces sent/rcvd are well formed */
-    int piece_len;
-
-    // TODO: need to remove this
-    int num_pieces;
-
-    /* info of who we are connected to */
-    void *peer_udata;
-
-    /* callbacks */
-    pwp_conn_cbs_t cb;
-    void *cb_ctx;
-
-    const sparsecounter_t *pieces_completed;
-
-} pwp_conn_private_t;
 
 static unsigned long __req_hash(const void *obj)
 {
